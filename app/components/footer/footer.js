f482aa2b401611f6e27b7e50b17b75c8fa607011
JJ Hiew <jj.hiew@gmail.com>
2015-10-26 14:55:34 -0400

    added footer

---
+++ app/components/footer/footer.js
@@ -0,0 +1,19 @@
+(function(){
+
+  'use strict';
+
+  angular.module('partnerApp')
+
+    .directive('appFooter', function(){
+
+      return {
+        scope: {},
+        bindToController: {},
+        controller: function() {},
+        controllerAs: 'ctrl',
+        templateUrl: 'components/footer/footer.html'
+      };
+
+    });
+
+})();
