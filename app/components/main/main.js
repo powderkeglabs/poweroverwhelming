c5884ac38d686acdfb56658b3c9ac9ac6074b2cf
JJ Hiew <jj.hiew@gmail.com>
2015-10-26 14:44:57 -0400

    initial commit

---
+++ app/components/main/main.js
@@ -0,0 +1,17 @@
+(function() {
+
+  'use strict';
+
+  angular.module('partnerApp')
+
+    .directive('mainApp', function(){
+      return {
+        scope: {},
+        bindToController: {},
+        controller: function() {},
+        controllerAs: 'ctrl',
+        templateUrl: 'components/main/main.html'
+      };
+    });
+
+})();
