8e03208f848cc4160ee0538995d1d607992e8ae2
JJ Hiew <jj.hiew@gmail.com>
2015-10-29 02:20:27 -0400

    sessions in firebase

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 
   angular.module('partnerApp', ['ui.router', 'firebase'])
 
+  .constant("FIREBASE_URL", "'https://crackling-inferno-4162.firebaseio.com/")
   .config(function($stateProvider, $urlRouterProvider){
 
     $urlRouterProvider.otherwise('/');
