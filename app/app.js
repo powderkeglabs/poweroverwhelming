c6e68105476b2b333fc788a1f4d20e8709e62589
JJ Hiew <jj.hiew@gmail.com>
2015-10-30 17:03:34 -0400

    add google analytics

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  angular.module('partnerApp', ['ui.router', 'firebase', 'ngSanitize'])
+  angular.module('partnerApp', ['ui.router', 'firebase', 'ngSanitize', 'angulartics', 'angulartics.google.analytics'])
 
   // .constant("FIREBASE_URL", "https://crackling-inferno-4162.firebaseio.com/")
   .constant("RACES", ['protoss', 'zerg', 'terran']);
