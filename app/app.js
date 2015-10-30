d890b514f2af7b78ba144c4160860559c1011234
JJ Hiew <jj.hiew@gmail.com>
2015-10-30 02:43:58 -0400

    cleanup app.js

--- app/app.js
+++ app/app.js
@@ -6,23 +6,5 @@
 
   .constant("FIREBASE_URL", "https://crackling-inferno-4162.firebaseio.com/")
   .constant("RACES", ['protoss', 'zerg', 'terran']);
-  // .config(function($stateProvider, $urlRouterProvider){
-
-    // $urlRouterProvider.otherwise('/');
-    // $stateProvider
-    //   .state('player-list', {
-    //     url: '/',
-    //     template: '<player-list></player-list>'
-    //   });
-
-
-      //g
-      // .state('done', {
-      //   url: '/done',
-      //   templateUrl: 'app/proxy/proxy-done.html',
-      //   controller: 'proxyDoneController',
-      //   params: { proxy: null }
-      // });
-  // });
 
 })();
