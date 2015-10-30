4f536c1756dfad9ab759f439644394be813fa20f
JJ Hiew <jj.hiew@gmail.com>
2015-10-30 16:18:11 -0400

    added santization and card layout updates

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  angular.module('partnerApp', ['ui.router', 'firebase'])
+  angular.module('partnerApp', ['ui.router', 'firebase', 'ngSanitize'])
 
   // .constant("FIREBASE_URL", "https://crackling-inferno-4162.firebaseio.com/")
   .constant("RACES", ['protoss', 'zerg', 'terran']);
